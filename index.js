583bd4497500e6936f07094ac84df78126db1ef6
lauren levaccare <laurenlevaccare@icloud.com>
2021-04-01 15:58:00 -0500

    secrete message fixed multi-hash bug

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
- let input = document.querySelector("#message-input");
+
+let { hash } = window.location;
+let message = atob(hash.replace('#', ''));
+    
+if (message) {
+    document.querySelector("#message-form").classList.add('hide');
+    document.querySelector("#message-show").classList.remove('hide');
+    document.querySelector('h1').innerHTML = message;
+}
+ 
 
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
+    document.querySelector('#message-form').classList.add('hide')
+    document.querySelector('#link-form').classList.remove('hide')
 
-    const encrypted = btoa(input.value)
-    document.querySelector("#link-input").value = encrypted; 
+    let input = document.querySelector("#message-input");
+    let encrypted = btoa(input.value)
+    let linkInput = document.querySelector("#link-input")
+    linkInput.value = `${window.location}#${encrypted}`; 
+    linkInput.select();
     
 }
 )
